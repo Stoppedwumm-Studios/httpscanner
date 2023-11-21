c03db4b156874eef8262dd4a7aeddaaf2f1c721d
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2023-11-21 14:28:14 +0100

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,46 @@
+import os
+
+os.system("pip3 install bs4")
+os.system("pip3 install requests")
+
+import json
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+def scan_website(url, depth=0,results=[]):
+    print(depth)
+    if depth >= 5:
+        return
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href:
+                results.append({
+                    "parent_url": url,
+                    "child_url": href
+                })
+
+                child_url = urljoin(url, href)
+                depth += 1
+                scan_website(child_url, depth, results)
+
+    else:
+        print(f"Error: Could not access website: {url}")
+
+if __name__ == "__main__":
+    
+    depth=0
+    url = input("Enter the website URL to scan: ")
+    results = []
+    scan_website(url, results=results)
+
+    with open('scan_results.json', 'w') as outfile:
+        json.dump(results, outfile)
+
+    print("Scan results saved to scan_results.json")
